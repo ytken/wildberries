dd051b301cb2e4404ca8b82e2048e9197a127092
ytken <1909dande.lion@gmail.com>
2022-06-23 00:48:36 +0500

    Added browser action to About screen

--- app/src/main/java/ru/ytken/wildberries/internship/week5okhttpmoshi/AboutFragment.kt
+++ app/src/main/java/ru/ytken/wildberries/internship/week5okhttpmoshi/AboutFragment.kt
@@ -3,6 +3,8 @@ package ru.ytken.wildberries.internship.week5okhttpmoshi
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context.CLIPBOARD_SERVICE
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +12,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import ru.ytken.wildberries.internship.week5okhttpmoshi.databinding.FragmentAboutBinding
+
 
 class AboutFragment: Fragment() {
     private lateinit var binding: FragmentAboutBinding
@@ -26,11 +29,9 @@ class AboutFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.imageViewCopyLink.setOnClickListener {
-            val clipboardManager = requireActivity().getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
-            val clipData = ClipData.newPlainText("label", "https://github.com/ytken/wildberries/tree/week_8_1")
-            clipboardManager.setPrimaryClip(clipData)
-            Toast.makeText(context, "Ссылка скопирована", Toast.LENGTH_SHORT).show()
+        binding.imageViewOpenInBrowser.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/ytken/wildberries/tree/week_8_1"))
+            startActivity(browserIntent)
         }
     }
 
